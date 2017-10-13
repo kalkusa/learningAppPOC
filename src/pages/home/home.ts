40b65fde126633e017ecd280e29f35a44d083991
kalkusa <kalkus@10g.pl>
2017-10-14 00:26:32 +0200

    Added new words to word list.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,7 +14,17 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
     this.wordList = [
       { Hanzi: "我", Pinyin: "Wǒ", English: "I" },
-      { Hanzi: "我们", Pinyin: "Wǒmen", English: "we" }
+      { Hanzi: "我们", Pinyin: "Wǒmen", English: "we" },
+      { Hanzi: "你", Pinyin: "Nǐ", English: "you" },
+      { Hanzi: "你们", Pinyin: "Nǐmen", English: "you (plural)" },
+      { Hanzi: "他", Pinyin: "Tā", English: "he" },
+      { Hanzi: "她", Pinyin: "Tā", English: "she" },
+      { Hanzi: "他们", Pinyin: "Tāmen", English: "they (males)" },
+      { Hanzi: "她们", Pinyin: "Tāmen", English: "they (females)" },
+      { Hanzi: "这 (这儿)", Pinyin: "Zhè (zhè'er)", English: "this (here, close)" },
+      { Hanzi: "那 (那儿)", Pinyin: "Nà (nà'er)", English: "that (there, away)" },
+      { Hanzi: "哪 (哪儿)", Pinyin: "Nǎ (nǎ'er)", English: "where (where)" },
+      { Hanzi: "谁", Pinyin: "Shéi", English: "who" }
     ];
     this.flashCardNumber = 0;
     this.isAnswerShowed = false; 
@@ -25,8 +35,12 @@ export class HomePage {
   }
 
   userKnowsWord() {
-    this.flashCardNumber++;
-    this.isAnswerShowed = false; 
+    if (this.flashCardNumber + 1 < this.wordList.length) {
+      this.flashCardNumber++;
+    } else {
+      this.flashCardNumber = 0;
+    }
+    this.isAnswerShowed = false;
   }
 
 }
--- www/build/main.js
+++ www/build/main.js
@@ -57,7 +57,17 @@ var HomePage = (function () {
         this.navCtrl = navCtrl;
         this.wordList = [
             { Hanzi: "我", Pinyin: "Wǒ", English: "I" },
-            { Hanzi: "我们", Pinyin: "Wǒmen", English: "we" }
+            { Hanzi: "我们", Pinyin: "Wǒmen", English: "we" },
+            { Hanzi: "你", Pinyin: "Nǐ", English: "you" },
+            { Hanzi: "你们", Pinyin: "Nǐmen", English: "you (plural)" },
+            { Hanzi: "他", Pinyin: "Tā", English: "he" },
+            { Hanzi: "她", Pinyin: "Tā", English: "she" },
+            { Hanzi: "他们", Pinyin: "Tāmen", English: "they (males)" },
+            { Hanzi: "她们", Pinyin: "Tāmen", English: "they (females)" },
+            { Hanzi: "这 (这儿)", Pinyin: "Zhè (zhè'er)", English: "this (here, close)" },
+            { Hanzi: "那 (那儿)", Pinyin: "Nà (nà'er)", English: "that (there, away)" },
+            { Hanzi: "哪 (哪儿)", Pinyin: "Nǎ (nǎ'er)", English: "where (where)" },
+            { Hanzi: "谁", Pinyin: "Shéi", English: "who" }
         ];
         this.flashCardNumber = 0;
         this.isAnswerShowed = false;
@@ -66,7 +76,12 @@ var HomePage = (function () {
         this.isAnswerShowed = true;
     };
     HomePage.prototype.userKnowsWord = function () {
-        this.flashCardNumber++;
+        if (this.flashCardNumber + 1 < this.wordList.length) {
+            this.flashCardNumber++;
+        }
+        else {
+            this.flashCardNumber = 0;
+        }
         this.isAnswerShowed = false;
     };
     return HomePage;
